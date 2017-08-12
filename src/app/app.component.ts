57cd093270b0ecb207042120fe97db7f02bcfc83
PoyChang <poypost@gmail.com>
2017-08-12 17:37:31 +0800

    使用 Swiper 建立輪播功能

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements AfterViewInit{
+  mySwiper: Swiper;
+  slides = [
+    'https://via.placeholder.com/300x200/FF5733/ffffff',
+    'https://via.placeholder.com/300x200/C70039/ffffff',
+    'https://via.placeholder.com/300x200/900C3F/ffffff'
+  ];
+
+  constructor() {}
+
+  ngAfterViewInit() {
+    this.mySwiper = new Swiper('.swiper-container', {
+      pagination: '.swiper-pagination',
+      paginationClickable: true,
+      nextButton: '.swiper-button-next',
+      prevButton: '.swiper-button-prev',
+      autoplay: 3000,
+      spaceBetween: 30
+    });
+  }
 }
